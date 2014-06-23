30bcb826c125708639ce820d35921e7663cf2d70
EC2 Default User <ec2-user@ip-172-31-40-1.us-west-2.compute.internal>
2014-06-23 19:13:31 +0000

    Data is parsing into json

--- tempServer.js
+++ tempServer.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http');
-
+var fs = require('fs');
 app.use(express.static(__dirname + '/public'));
 
 var port = 80;
@@ -20,8 +20,23 @@ io.on('connection', function(socket) {
     console.log('user disconnected');
   });
 
-  socket.on('brainData', function(data) {
-    console.log(data);
-  });
+var brainData = {};  
+socket.on('brainData', function(data) {
+   var parsed = data.split(',');  
+   brainData.poorSignalValue = parseInt(parsed[0]);
+   brainData.attention = parseInt(parsed[1]);
+   brainData.meditation = parseInt(parsed[2]);
+   brainData.delta = parseInt(parsed[3]);
+   brainData.theta = parseInt(parsed[4]);
+   brainData.lowAlpha = parseInt(parsed[5]);
+   brainData.highAlpha = parseInt(parsed[6]);
+   brainData.lowBeta = parseInt(parsed[7]);
+   brainData.highBeta = parseInt(parsed[8]);
+   brainData.lowGamma = parseInt(parsed[9]);
+   brainData.highGamma = parseInt(parsed[10]);
+
+   console.log(brainData);   
+
+});
 
 });
